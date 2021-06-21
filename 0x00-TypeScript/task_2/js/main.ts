25d3e368be2d4dcd08e4e1cd4fb95bfec902c9fc
veeteeran <vietnamtran@gmail.com>
2021-06-21 16:03:45 -0500

    Add teachClass function

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -63,3 +63,10 @@ const executeWork = (employee: Director | Teacher) => {
     console.log(employee.workTeacherTasks());
   }
 }
+
+type Subjects = 'Math' | 'History';
+
+const teachClass = (todayClass: string): string => {
+  if (todayClass === 'Math') { return 'Teaching Math' }
+  else if (todayClass === 'History') { return 'Teaching History' }
+}
