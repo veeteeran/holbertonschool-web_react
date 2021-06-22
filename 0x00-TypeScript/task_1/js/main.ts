bd10da4551f69d37b1d259075369d4b5768d97c0
veeteeran <vietnamtran@gmail.com>
2021-06-22 06:32:14 -0500

    Fix typo in printTeacher function

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -11,7 +11,7 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-const printTeacher = (firstName: string, lastName: string): string => `${firstName[0]} ${lastName}`;
+const printTeacher = (firstName: string, lastName: string): string => `${firstName[0]}. ${lastName}`;
 
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
