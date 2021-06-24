df6e4f8e0153f2af57ca9f9100c33e63210552c0
veeteeran <vietnamtran@gmail.com>
2021-06-24 07:11:48 -0500

    Rewrite Jquery

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,10 +1,5 @@
 import $ from "jquery";
 
-const createParagraphs = () => {
-  const p1 = $('<p></p>').text('Holberton Dashboard');
-  const p2 = $('<p></p>').text('Dashboard data for the students');
-  const p3 = $('<p></p>').text('Copyright - Holberton School');
-  $('body').append(p1, p2, p3);
-}
-
-$(document).ready(() => createParagraphs());
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
