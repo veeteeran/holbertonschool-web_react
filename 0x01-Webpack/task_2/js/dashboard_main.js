dccb92cb0f0598c9a5e88e6cefff8ab4f36140dd
veeteeran <vietnamtran@gmail.com>
2021-06-24 07:40:20 -0500

    Add task_2

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+const _ = require('lodash');
+
+const p1 = $('<p></p>').text('Holberton Dashboard');
+const p2 = $('<p></p>').text('Dashboard data for the students');
+const button = $('<button></button>').text('Click here to get started');
+const p3 = $('<p></p>').prop('id', 'count');
+const p4 = $('<p></p>').text('Copyright - Holberton School');
+const img = $('<img>').prop('id', 'logo').width(200).height(200);
+
+$('body').append(p1, p2, button, p3, p4);
+
+let count = 0;
+const updateCounter = () => {
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter))
---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["optipng", { optimizationLevel: 5 }],
+          // Svgo configuration here https://github.com/svg/svgo#configuration
+          [
+            "svgo",
+            {
+              plugins: extendDefaultPlugins([
+                {
+                  name: "removeViewBox",
+                  active: false,
+                },
+                {
+                  name: "addAttributesToSVGElement",
+                  params: {
+                    attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+                  },
+                },
+              ]),
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+};
