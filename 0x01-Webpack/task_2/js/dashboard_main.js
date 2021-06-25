df374f4351a3468a71b9aa78c3c7ac38d4d43e63
veeteeran <vietnamtran@gmail.com>
2021-06-24 19:39:03 -0500

    Add files

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -7,8 +7,9 @@ const button = $('<button></button>').text('Click here to get started');
 const p3 = $('<p></p>').prop('id', 'count');
 const p4 = $('<p></p>').text('Copyright - Holberton School');
 const img = $('<img>').prop('id', 'logo').width(200).height(200);
+img.prop('src', '../assets/holberton-logo.jpg')
 
-$('body').append(p1, p2, button, p3, p4);
+$('body').append(img, p1, p2, button, p3, p4);
 
 let count = 0;
 const updateCounter = () => {
