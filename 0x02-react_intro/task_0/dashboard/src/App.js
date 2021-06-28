59503aaadf6c9fa87a972a167c31819907e60f46
veeteeran <vietnamtran@gmail.com>
2021-06-28 09:35:10 -0500

    Add task 0

---
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import holberton_logo from './holberton_logo.jpeg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton_logo} className="App-logo" alt="logo" />
+        <h1 className="title">School dashboard</h1>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+      <hr className="hr" />
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+      <hr className="hr" />
+      <footer className="App-footer">Copyright 2020 - holberton School</footer>
+    </div>
+  );
+}
+
+export default App;
