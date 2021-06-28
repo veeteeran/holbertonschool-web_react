bb7eb9907c08a0b34093b9d99449585d1fc4322e
veeteeran <vietnamtran@gmail.com>
2021-06-28 17:41:55 -0500

    Delete unused imports

--- 0x02-react_intro/task_3/dashboard/src/Notifications.test.js
+++ 0x02-react_intro/task_3/dashboard/src/Notifications.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Notifications from './Notifications';
-import closeIcon from './close-icon.png';
-import { getLatestNotification } from './utils';
 import { shallow } from 'enzyme';
 
 describe('rendering components', () => {
