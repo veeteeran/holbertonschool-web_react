bbc87293fb49f2c4b42ecc50125a8e23bdde867a
veeteeran <vietnamtran@gmail.com>
2021-07-03 13:34:43 -0500

    Refactor tests

--- 0x02-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Notifications from './Notifications';
+import { getLatestNotification } from '../utils/utils';
 import { shallow } from 'enzyme';
 
 describe('rendering components', () => {
   it('renders Notifications component without crashing', () => {
-    shallow(<Notifications />);
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.exists()).toBe(true);
   });
 
   it('renders an unordered list', () => {
     const wrapper = shallow((<Notifications />));
     expect(wrapper.find('ul').children()).toHaveLength(3);
+    expect(wrapper.find('ul').childAt(0).html()).toEqual('<li data-default=\"true\">New course available</li>');
+    expect(wrapper.find('ul').childAt(1).html()).toEqual('<li data-urgent=\"true\">New resume available</li>');
+    expect(wrapper.find('ul').childAt(2).html()).toEqual(`<li data-urgent=\"true\">${getLatestNotification()}</li>`);
   });
 
   it('renders the text "Here is the list of notifications"', () => {
