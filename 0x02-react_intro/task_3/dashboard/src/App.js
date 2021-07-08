c6498b58aad7575976fb3bf659d98e2a050b4639
veeteeran <vietnamtran@gmail.com>
2021-07-07 19:24:26 -0500

    Add missing files

--- 0x02-react_intro/task_3/dashboard/src/App.js
+++ 0x02-react_intro/task_3/dashboard/src/App.js
@@ -5,21 +5,21 @@ import { getFullYear, getFooterCopy } from './utils';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={holberton_logo} className="App-logo" alt="logo" />
         <h1 className="title">School dashboard</h1>
-      </header>
+      </div>
       <hr className="hr" />
-      <body className="App-body">
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
         <label htmlFor="email">Email: </label>
         <input type="email" id="email" name="email" />
         <label htmlFor="password">Password: </label>
         <input type="password" id="password" name="password" />
         <button>OK</button>
-      </body>
+      </div>
       <hr className="hr" />
-      <footer className="App-footer">Copyright {getFullYear()} - {getFooterCopy(true)}</footer>
+      <div className="App-footer">Copyright {getFullYear()} - {getFooterCopy(true)}</div>
     </div>
   );
 }
