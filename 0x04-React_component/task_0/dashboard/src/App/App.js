0f6a5804fde259481b5356a0b70ce5ca28201560
veeteeran <vietnamtran@gmail.com>
2021-07-10 08:19:53 -0500

    Move variables inside class definition

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import Notifications from '../Notifications/Notifications';
-import NotificationItem from '../Notifications/NotificationItem';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -9,30 +8,31 @@ import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 
-const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 }
-];
-
-const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'default', html: getLatestNotification() }
-];
 
 class App extends React.Component {
+  listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 }
+  ];
+
+  listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'default', html: getLatestNotification() }
+  ];
+
   render() {
     return (
       <>
         <div className='container'>
           <Header />
-          <Notifications listNotifications={listNotifications} />
+          <Notifications listNotifications={this.listNotifications} />
         </div>
         <hr className="hr" />
         {
           this.props.isLoggedIn
-            ? <CourseList listCourses={listCourses} />
+            ? <CourseList listCourses={this.listCourses} />
             : <Login />
         }
         <hr className="hr" />
