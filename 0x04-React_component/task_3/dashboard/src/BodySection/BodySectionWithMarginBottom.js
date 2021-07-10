271ebdd87784bad97c92cf5503ff52d6abb395b5
veeteeran <vietnamtran@gmail.com>
2021-07-10 17:23:58 -0500

    Render children

--- 0x04-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -4,9 +4,11 @@ import './BodySectionWithMarginBottom.css';
 
 export default class BodySectionWithMarginBottom extends Component {
   render() {
+    const { children } = this.props
     return (
       <div className='bodySectionWithMargin'>
         <BodySection />
+        {children}
       </div>
     );
   }
