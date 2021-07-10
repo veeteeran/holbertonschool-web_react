994ae839c332491d7b84dd8e51cc347ca8ccbc63
veeteeran <vietnamtran@gmail.com>
2021-07-10 16:44:04 -0500

    Refactor to class component, no tests

---
+++ 0x04-React_component/task_1/dashboard/src/App.test.js
@@ -0,0 +1,82 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import App from './App/App';
+import Notifications from './Notifications/Notifications';
+import Login from './Login/Login';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import CourseList from './CourseList/CourseList';
+import { shallow, mount } from 'enzyme';
+
+describe('rendering components', () => {
+  it('renders App component without crashing', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('App contains Notifications component', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find(Notifications)).toHaveLength(1);
+  });
+
+  it('App contains Header component', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.contains(<Header />)).toBe(true);
+  });
+
+  it('App contains Login component', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.contains(<Login />)).toBe(true);
+  });
+
+  it('App contains Footer component', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.contains(<Footer />)).toBe(true);
+  });
+
+  it('checks CourseList is not rendered', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.contains(<CourseList />)).toBe(false);
+  });
+});
+
+describe('when isLogged in is true', () => {
+  const wrapper = shallow(<App isLoggedIn={true} />);
+
+  it('checks Login is not rendered', () => {
+    expect(wrapper.contains(<Login />)).toBe(false);
+  });
+
+  it('checks CourseList is rendered', () => {
+    expect(wrapper.find(CourseList)).toHaveLength(1);
+  })
+});
+
+describe('when Ctrl+h pressed', () => {
+  it('checks logOut function is called', () => {
+    // const wrapper = mount(<App />).instance()
+    // const spy = jest.spyOn(instance, 'logOut');
+    // const spy = jest.spyOn(App.prototype, 'alert')
+    const mockFn = jest.fn()
+    const wrapper = mount(<App logOut={mockFn} />);
+    wrapper.simulate('keydown', { key: 'Control' })
+    // expect(mockFn).toHaveBeenCalled();
+    // wrapper.mockRestore();
+  });
+
+  // it('checks alert function is called', () => {
+
+  // });
+
+  // it('checks alert string is "Logging you out"', () => {
+
+  // });
+});
