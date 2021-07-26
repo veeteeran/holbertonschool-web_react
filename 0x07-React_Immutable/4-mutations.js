67535f80a3b72f37cbc8289e1187066b26525ddb
veeteeran <vietnamtran@gmail.com>
2021-07-26 11:03:32 -0500

    Add task_4

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+const { Map } = require('immutable');
+
+const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+const map2 = map.withMutations(map => {
+  map.set(2, 'Benjamin').set(4, 'Oliver')
+})
+
+module.exports = { map, map2 };
