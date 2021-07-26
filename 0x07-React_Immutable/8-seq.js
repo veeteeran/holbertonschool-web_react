c054bfc6af553dc57491750218c71e974e7893cc
veeteeran <vietnamtran@gmail.com>
2021-07-26 15:37:03 -0500

    Add task 8

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const { Seq } = require('immutable');
+
+const printBestStudents = object => {
+  const students = Seq(object)
+    .filter(x => x.score >= 70)
+    .map(
+      x => ({
+        score: x.score,
+        firstName: x.firstName.charAt(0).toUpperCase() + x.firstName.slice(1),
+        lastName: x.lastName.charAt(0).toUpperCase() + x.lastName.slice(1)
+      }));
+
+  console.log(students.toObject())
+}
+
+module.exports = printBestStudents;
+
+const grades = {
+  1: {
+    score: 99,
+    firstName: 'guillaume',
+    lastName: 'salva',
+  }
+};
+
+printBestStudents(grades)
