9a126b4b340510e48a008f61146e7f340f5afeae
veeteeran <vietnamtran@gmail.com>
2021-07-26 10:42:10 -0500

    Add task_3

--- 0x07-React_Immutable/3-list.js
+++ 0x07-React_Immutable/3-list.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+const { List } = require('immutable');
+
+function getListObject(array) {
+  return List(array);
+};
+
+function addElementToList(list, element) {
+  return list.push(element)
+};
+
+module.exports = { getListObject, addElementToList };
