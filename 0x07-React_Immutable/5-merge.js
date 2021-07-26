82e8678ed59eeb4dea09ab9048b3ed07328727e3
veeteeran <vietnamtran@gmail.com>
2021-07-26 14:58:50 -0500

    Add task 5

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+const { List, Map } = require('immutable');
+
+const concatElements = (page1, page2) => List(page1).concat(List(page2))
+
+const mergeElements = (page1, page2) => List(Map(page1).merge(Map(page2)))
+
+module.exports = { concatElements, mergeElements }
