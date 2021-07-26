16fba45ddf6c44ee5a90b503904bde74f3a422f8
veeteeran <vietnamtran@gmail.com>
2021-07-26 09:40:56 -0500

    Add task 0

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+const { fromJS } = require('immutable');
+// import { fromJS } from 'immutable';
+
+const getImmutableObject = object => fromJS(object);
+
+module.exports = getImmutableObject;
