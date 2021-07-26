db6b8c7af307b75dbbec8e61ad480bead6d58aea
veeteeran <vietnamtran@gmail.com>
2021-07-26 09:48:14 -0500

    Add task 1

--- 0x07-React_Immutable/1-map.js
+++ 0x07-React_Immutable/1-map.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const { fromJS, Map } = require('immutable');
+
+const getImmutableObject = object => Map(object);
+
+module.exports = getImmutableObject;
+
+const foo = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+}
+
+console.log(getImmutableObject(foo))
