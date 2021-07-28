019da4586a20e9d2dbb3f8a312fb55e030f2c335
veeteeran <vietnamtran@gmail.com>
2021-07-27 20:10:29 -0500

    Fix formatting

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,6 +1,5 @@
 const { fromJS } = require('immutable');
-// import { fromJS } from 'immutable';
 
-const getImmutableObject = object => fromJS(object);
+const getImmutableObject = (object) => fromJS(object);
 
 module.exports = getImmutableObject;
