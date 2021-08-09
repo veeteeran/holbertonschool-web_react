9948b6713558546e0759e1a0d41dd81779b7e46e
veeteeran <vietnamtran@gmail.com>
2021-08-08 21:28:55 -0500

    Refactor uiReducer

--- 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -1,6 +1,6 @@
 import uiReducer, { initialState } from './uiReducer'
-import { SELECT_COURSE } from '../actions/courseActionTypes'
-import { DISPLAY_NOTIFICATION_DRAWER } from '../actions/uiActionTypes'
+import { selectCourse } from '../actions/courseActionCreators'
+import { displayNotificationDrawer } from '../actions/uiActionCreator'
 
 describe('uiReducer', () => {
   it('Verifies initial state returned when no action is passed to uiReducer', () => {
@@ -9,12 +9,12 @@ describe('uiReducer', () => {
   })
 
   it('Verifies the state equals the initial state when the action SELECT_COURSE is passed', () => {
-    const myState = uiReducer(initialState, SELECT_COURSE)
+    const myState = uiReducer(initialState, selectCourse())
     expect(myState.toJS()).toEqual(initialState.toJS())
   })
 
   it('Verifies property isNotificationDrawerVisible is true when DISPLAY_NOTIFICATION_DRAWER passed', () => {
-    const myState = uiReducer(initialState, DISPLAY_NOTIFICATION_DRAWER)
+    const myState = uiReducer(initialState, displayNotificationDrawer())
     expect(myState.toJS().isNotificationDrawerVisible).toEqual(true)
   })
 })
